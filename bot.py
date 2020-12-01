c7d575ba45c0e68b33abe5b00aaf40511b163fc0
V4SHADY <74726621+V4SHADY@users.noreply.github.com>
2020-12-01 13:02:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,16 @@
 import discord
 from discord.ext import commands
 
+client = commands.Bot(command_prefix=">")
+
 @client.event
 async def on_ready():
     print("Bot is Ready")
+
+@client.command
+async def hello(ctx):
+    await ctx.send("Hi!")
+
+
+
+client.run('Your Bot Token')
